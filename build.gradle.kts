1eb7a54da9816c1035546886cff6de8f3b76951e
Ivan Priala <ipriala37@gmail.com>
2026-05-07 21:20:38 +0200

    fix: remove explicit sonar paths to avoid double indexing

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,5 @@ sonar {
         property("sonar.projectKey", "JOVANNIZ10_com.lksnext.ParkingIPriala")
         property("sonar.organization", "jovanniz10")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.tests", "app/src/test/java")
-        property("sonar.java.binaries", "app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes")
-        property("sonar.android.lint.report", "app/build/reports/lint-results-debug.xml")
-        property("sonar.junit.reportPaths", "app/build/test-results/debugUnitTest/")
-        property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/debug/jacoco.xml")
     }
 }
