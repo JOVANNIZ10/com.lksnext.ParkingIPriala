f36d98467e8ad0f56a436657fe08602f424d2f7f
Ivan Priala <ipriala37@gmail.com>
2026-05-07 20:58:51 +0200

    fix: improve sonarqube config with kotlin sources and coverage paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,12 @@ sonar {
         property("sonar.projectKey", "JOVANNIZ10_com.lksnext.ParkingIPriala")
         property("sonar.organization", "jovanniz10")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.java.binaries", "app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes")
+        property("sonar.sources", "app/src/main/java,app/src/main/kotlin")
+        property("sonar.tests", "app/src/test/java,app/src/test/kotlin")
+        property("sonar.java.binaries", "app/build/intermediates/classes/debug")
+        property("sonar.kotlin.binaries", "app/build/intermediates/classes/debug")
         property("sonar.android.lint.report", "app/build/reports/lint-results-debug.xml")
+        property("sonar.junit.reportPaths", "app/build/test-results/debugUnitTest/")
+        property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/debug/jacoco.xml")
     }
 }
